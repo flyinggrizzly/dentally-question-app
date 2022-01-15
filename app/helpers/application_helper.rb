fd23ad6d67616b74a84e7ef7d499abdff4ba2b21
Sean DMR <sean@flyinggrizzly.net>
2022-01-15 14:43:29 +0000

    Set up flash and nav of main application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def bootstrap_flash_class(level)
+    case level
+    when :notice
+      "flash alert alert-info"
+    when :success
+      "flash alert alert-success"
+    when :error
+      "flash alert alert-error"
+    when :alert
+      "flash alert alert-error"
+    else
+      "flash alert"
+    end
+  end
 end
